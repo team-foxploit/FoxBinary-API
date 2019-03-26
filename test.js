5f0aeab365b685e9c8869e7044c516c8658350de
luk3Sky <ganindudananja@gmail.com>
2019-03-27 00:14:52 +0530

    Node.js code for websocket created

---
+++ test.js
@@ -0,0 +1,11 @@
+var WebSocket = require('websocket').w3cwebsocket;
+var ws = new WebSocket('wss://ws.binaryws.com/websockets/v3?app_id=1089');
+
+ws.onopen = function(evt) {
+    ws.send(JSON.stringify({ticks:'R_100'}));
+};
+
+ws.onmessage = function(msg) {
+   var data = JSON.parse(msg.data);
+   console.log('ticks update: %o', data);
+};

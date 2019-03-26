b44b402c2ac2a2abb7cb65e4b90ce7afa7602901
luk3Sky <ganindudananja@gmail.com>
2019-03-27 00:15:06 +0530

    Changed Data.py

--- data.py
+++ data.py
@@ -1,3 +1,4 @@
+import websocket
 from websocket import create_connection
 import json
 
@@ -11,18 +12,18 @@ def on_message(ws, message):
     print('ticks update: %s' % message)
     # y = json.loads(message)
     # print(y)
-    # return [message[key] for key in sorted(message.keys())]
+    return [message[key] for key in sorted(message.keys())]
 
 def run():
     apiUrl = "wss://ws.binaryws.com/websockets/v3?app_id=1089"
-    # ws = websocket.WebSocketApp(apiUrl, on_message = on_message, on_open = on_open)
-    # ws.run_forever()
-    ws = create_connection(apiUrl)
-    while(1):
-        on_open(ws)
-        result =  ws.recv()
-        print("Received '%s'" % result)
-        return json.loads(result)
+    ws = websocket.WebSocketApp(apiUrl, on_message = on_message, on_open = on_open)
+    ws.run_forever()
+#     ws = create_connection(apiUrl)
+#     while(1):
+#         on_open(ws)
+#         result =  ws.recv()
+#         print("Received '%s'" % result)
+#         return json.loads(result)
 
 
 

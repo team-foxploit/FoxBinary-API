a42cdf11a6de708107525060eb0cbf344403847a
luk3Sky <ganindudananja@gmail.com>
2019-04-06 17:33:55 +0530

    Create serializers.py

---
+++ foxbinaryapi_manager/foxbinaryapi/serializers.py
@@ -0,0 +1,8 @@
+from foxbinaryapi.models import TickHistory
+from rest_framework import serializers
+
+# TickHistory seralizer
+class TickHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model: TickHistory
+        fields: = '__all__'

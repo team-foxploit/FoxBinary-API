a730a3c58042b0fb840393124f58301f69ce233b
luk3Sky <ganindudananja@gmail.com>
2019-04-06 21:08:17 +0530

    Update views.py

--- foxbinaryapi_manager/foxbinaryapi/views.py
+++ foxbinaryapi_manager/foxbinaryapi/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from foxbinaryapi.models import TickHistory
+from foxbinaryapi.serializers import TickHistorySerializer
 
 # Create your views here.
+class TickHistoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows tick history to be viewed or edited.
+    """
+    queryset = TickHistory.objects.all().order_by('-date_joined')
+    serializer_class = TickHistorySerializer

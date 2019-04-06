4a801319937e089c088dc7441c7ee89a5c728505
luk3Sky <ganindudananja@gmail.com>
2019-04-06 17:25:48 +0530

    Model created

--- foxbinaryapi_manager/foxbinaryapi/models.py
+++ foxbinaryapi_manager/foxbinaryapi/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class TickHistory(models.Model):
+    prices = models.IntegerField()
+    times = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
